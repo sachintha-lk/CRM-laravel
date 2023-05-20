ef5df6009752c54fd43a73fa6916f4e1ce5379ce
Sachintha <68807141+sachintha-lk@users.noreply.github.com>
2023-05-20 19:04:09 +0530

    fix semicolon bug

--- resources/views/components/dashboard/navlinks.blade.php
+++ resources/views/components/dashboard/navlinks.blade.php
@@ -1,7 +1,7 @@
 @if(Auth::User())
 
     {{-- Only admin can manage the users at the moment --}}
-    @if(Auth::User()->role()->first()->name == 'Admin');
+    @if(Auth::User()->role()->first()->name == 'Admin')
         <x-nav-link href="{{ route('manageusers') }}" :active="request()->routeIs('manageusers')">
             {{ __('Manage Users') }}
         </x-nav-link>
