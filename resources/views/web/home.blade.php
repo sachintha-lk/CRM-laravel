627d87f0961a3b371045f7ba2d6ba9cdbd393f8e
Sachintha <68807141+sachintha-lk@users.noreply.github.com>
2023-05-21 12:50:13 +0530

    Added Offers section

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -169,6 +169,49 @@ Book Now      </a>
   
 
     </section>
+    
+<section class=" w-3/4 p-3 mx-auto pt-5">
+<div>
+  <div class="text-center text-4xl font-semibold text-pink-500 m-2">Offers</div>
+</div>
+<div class="flex gap-10 ">
+  <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">15% Off for Combank Credit Cards</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 ">Offer Valid till 30th Of July 2023</p>
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-500 rounded-lg hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 ">
+        View Offer
+        <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+  </div>
+  
+  <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">12% Off for Promo Code: HAIR2023</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 ">Offer Valid till 30th Of July 2023</p>
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-500 rounded-lg hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 ">
+        View Offer
+        <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+  </div>
+  
+  <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">15% Off for Frimi</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 ">Offer Valid till 30th Of July 2023</p>
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-500 rounded-lg hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 ">
+        View Offer
+        <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+  </div>
+
+</div>
+
+
+    </section>
 
 
     {{-- Gallery --}}
@@ -383,7 +426,7 @@ Book Now      </a>
           <strong class="mr-1">Limited offer!</strong> Get massive discounts now before it's to late
         </div>
         <div class="flex items-center justify-center">
-          <a class="inline-block px-6 py-2.5 bg-white text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-100 hover:shadow-lg focus:bg-gray-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-200 active:shadow-lg transition duration-150 ease-in-out mr-4" href="#" role="button" data-mdb-ripple="true" data-mdb-ripple-color="light">Claim offer</a>
+          <a class="inline-block px-6 py-2.5 bg-white text-gray-700 font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-100 hover:shadow-lg focus:bg-gray-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-200 active:shadow-lg transition duration-150 ease-in-out mr-4" href="#" role="button" data-mdb-ripple="true" data-mdb-ripple-color="light">Claim offer</a>
  
 
           <div class="text-white" data-bs-dismiss="alert" aria-label="Close" id="offer-banner-close">
